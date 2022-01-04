2bb0fee541b2ec1d8e37cfc7b2e0e141f4f4edc3
Vuinea <87769545+Vuinea@users.noreply.github.com>
2022-01-04 18:03:33 -0500

    added the modal on the monthly view (haven't written backend)

--- app/static/script.js
+++ app/static/script.js
@@ -19,3 +19,38 @@ const displayExtraInfo = (item) => {
   }
 }
 
+// the modal on the month view
+
+
+const modal = document.getElementById("add-item-modal");
+const closeBtn = document.getElementsByClassName("close")[0];
+const dateInput = document.getElementById("modal-add-due");
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+}
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+function changeValue(date) {
+  const splitDate = date.split("-");
+  const day = splitDate[2];
+  const month = splitDate[1]
+
+  if (day.length === 1) {
+    splitDate[2] = "0" + day;
+  }
+
+  if (month.length === 1) {
+    splitDate[1] = "0" + month;
+  }
+
+  date = splitDate.join("-")
+  dateInput.value = date;
+  modal.style.display = "block"
+
+}
